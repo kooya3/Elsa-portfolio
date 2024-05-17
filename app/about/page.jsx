5d1256c42af4f18de2bd4bbe0c093f46df4bb37f
Elyees Tatua <elyeestatua@gmail.com>
2024-05-18 00:21:11 +0300

    refactor(home): redesign about page

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -22,17 +22,17 @@ const projects = [
   },
 ];
 
-function page() {
+function Page() {
   return (
     <motion.div
       initial={{ y: 100, opacity: 0 }}
       animate={{ y: 0, opacity: 1, transition: { duration: 0.2 } }}
-      className="bg-white top-24 relative dark:bg-[#212121] shadow-md rounded-xl max-w-xl lg:mx-auto mx-4 lg:max-w-xl  max-2xl  md:mx-auto sm:mx-auto lg:px-0 "
+      className="bg-white top-24 relative dark:bg-[#212121] shadow-md rounded-xl max-w-xl lg:mx-auto mx-4 lg:max-w-xl max-2xl md:mx-auto sm:mx-auto lg:px-0"
     >
-      <div className=" lg:p-6 p-5 ">
+      <div className="lg:p-6 p-5">
         <div className="flex mt-4 items-center gap-x-2 font-InterRe">
-          <div className="w-[6px] h-[6px] rounded-full  bg-[#FB6D48]" />
-          <h1 className="text-neutral-700  dark:text-[#C0C0C0] text-lg font-InterMe">
+          <div className="w-[6px] h-[6px] rounded-full bg-[#FB6D48]" />
+          <h1 className="text-neutral-700 dark:text-[#C0C0C0] text-lg font-InterMe">
             About
           </h1>
         </div>
@@ -50,12 +50,11 @@ function page() {
             }}
             className="text-neutral-700 text-lg my-7 leading-8 font-InterRe dark:text-[#C0C0C0]"
           >
-          Elsarita Njoki Oulie is a dedicated and organized individual with good collaborative skills. She is seeking a job that will allow her to make the best use of her skills and further develop them. Elsarita has a genuine interest in the transport/logistics industry and considers her strengths to be long-term planning skills and the ability to complete tasks within deadlines. She is a team player who thrives in collaboration with others and contributes to a positive and pleasant working environment. Elsarita also has a strong sense of social responsibility and is involved in the Red Cross. Her outdoor activities include skiing, cycling, sailing, and participating in triathlons.
-        
+            I am Elsarita Njoki Oulie, a dedicated and organized individual with good collaborative skills. I am seeking a job that will allow me to make the best use of my skills and further develop them. I have a genuine interest in the transport/logistics industry and consider my strengths to be long-term planning skills and the ability to complete tasks within deadlines. I am a team player who thrives in collaboration with others and contributes to a positive and pleasant working environment. I also have a strong sense of social responsibility and am involved in the Red Cross. My outdoor activities include skiing, cycling, sailing, and participating in triathlons.
           </motion.p>
         </div>
 
-        <div className="p-4  rounded-lg dark:bg-[#373737] bg-white border dark:border-neutral-700/70">
+        <div className="p-4 rounded-lg dark:bg-[#373737] bg-white border dark:border-neutral-700/70">
           <motion.img
             initial={{ y: 100, opacity: 0 }}
             animate={{
@@ -91,8 +90,8 @@ function page() {
             }}
             className="text-neutral-700 text-lg my-7 leading-8 font-InterRe dark:text-[#C0C0C0]"
           >
-            I hold a Bachelor's degree in Information Technology from Jomo Kenyatta University of Agriculture and Technology. I have a strong background in software, databases, and networks. With my knowledge in IT, I have stayed up-to-date with the latest technologies and industry trends. I actively seek opportunities to expand my skills and I regularly participate in IT conferences and workshops. My dedication to professional development demonstrates my relentless drive to stay current in the ever-evolving field of technology.
-           </motion.p>
+            I hold a Bachelor's degree in Information Technology from Jomo Kenyatta University of Agriculture and Technology. I have a strong background in software, databases, and networks. With my knowledge in IT, I have stayed up-to-date with the latest technologies and industry trends. I actively seek opportunities to expand my skills and regularly participate in IT conferences and workshops. My dedication to professional development demonstrates my relentless drive to stay current in the ever-evolving field of technology.
+          </motion.p>
 
           <motion.p
             initial={{ y: 100, opacity: 0 }}
@@ -103,8 +102,8 @@ function page() {
             }}
             className="text-neutral-700 text-lg my-7 leading-8 font-InterRe dark:text-[#C0C0C0]"
           >
-            When I'm not immersed in my work in the IT field, I find solace in outdoor activities such as skiing, cycling, sailing, and participating in triathlons. I believe in maintaining a healthy work-life balance as well as understanding the importance of taking breaks and reenergizing my mind and body. In my spare time, I also volunteer at the Red Cross, contributing to society and helping those in need. Additionally, I enjoy exploring nature and spending time with animals, which aligns with my passion for outdoor activities and my involvement in the Red Cross.
-            </motion.p>
+            When I'm not immersed in my work in the IT field, I find solace in outdoor activities such as skiing, cycling, sailing, and participating in triathlons. I believe in maintaining a healthy work-life balance and understand the importance of taking breaks and reenergizing my mind and body. In my spare time, I also volunteer at the Red Cross, contributing to society and helping those in need. Additionally, I enjoy exploring nature and spending time with animals, which aligns with my passion for outdoor activities and my involvement in the Red Cross.
+          </motion.p>
 
           <motion.h1
             initial={{ x: -100, opacity: 0 }}
@@ -130,18 +129,18 @@ function page() {
           </motion.p>
         </div>
 
-        <div className="dark:bg-[#2C2C2C] bg-[#F6F8FA] p-4  mt-6 rounded-md">
+        <div className="dark:bg-[#2C2C2C] bg-[#F6F8FA] p-4 mt-6 rounded-md">
           <div className="">
             <Link href={"https://www.linkedin.com/in/elsarita-njoki-oulie-b22b3a291/"}>
               {projects.map((link) => (
                 <div
                   key={link.subtitle}
-                  className="dark:bg-[#373737] bg-white mt-3 rounded-xl shadow-md flex  max-md:flex-col max-md:items-start items-center justify-between p-5 "
+                  className="dark:bg-[#373737] bg-white mt-3 rounded-xl shadow-md flex max-md:flex-col max-md:items-start items-center justify-between p-5"
                 >
-                  <div className="flex  items-center gap-x-4">
-                    <div className="drop-shadow-md ">
+                  <div className="flex items-center gap-x-4">
+                    <div className="drop-shadow-md">
                       <img
-                        className="w-10 h-10 object-cover rounded-full "
+                        className="w-10 h-10 object-cover rounded-full"
                         src={link.img}
                         alt=""
                       />
@@ -152,7 +151,7 @@ function page() {
                   </div>
 
                   <div className="flex max-md:mt-4 w-fit items-center gap-x-3">
-                    <p className="text-sm font-semibold uppercase text-neutral-40 bg-[#EDEFF3] dark:bg-neutral-600/50 dark:text-neutral-400 text-[#666D80]  px-2 rounded-lg ">
+                    <p className="text-sm font-semibold uppercase text-neutral-40 bg-[#EDEFF3] dark:bg-neutral-600/50 dark:text-neutral-400 text-[#666D80] px-2 rounded-lg">
                       {link.subtitle}
                     </p>
                     <div className="">
@@ -165,10 +164,10 @@ function page() {
           </div>
         </div>
 
-        <div className="mt-24 lg:p-2 ">
+        <div className="mt-24 lg:p-2">
           <div className="mx-auto max-w-md">
             <div>
-              <h2 className=" text-4xl text-center dark:text-white text-[#353849] font-InterBo">
+              <h2 className="text-4xl text-center dark:text-white text-[#353849] font-InterBo">
                 Let’s work together.
               </h2>
               <p className="text-center mt-3 dark:text-neutral-300 text-[#666D80] text-lg">
@@ -178,11 +177,11 @@ function page() {
 
             <div className="">
               <div className="flex items-center gap-x-4 justify-center mt-6">
-                <div className="bg-black dark:bg-[#373737] w-fit shadow-md  px-2 rounded-md flex items-center gap-x-2 py-1">
+                <div className="bg-black dark:bg-[#373737] w-fit shadow-md px-2 rounded-md flex items-center gap-x-2 py-1">
                   <AiOutlinePlusCircle className="text-white tex-3" />
-                  <a href="mailto:your-email@example.com"> <span className="text-white  font-semibold">Hire Me</span> </a>
+                  <a href="mailto:your-email@example.com"> <span className="text-white font-semibold">Hire Me</span> </a>
                 </div>
-                <div className="border-neutral-300 dark:border-neutral-700 shadow-md border  w-fit h-8 px-2 rounded-md flex items-center gap-x-2 ">
+                <div className="border-neutral-300 dark:border-neutral-700 shadow-md border w-fit h-8 px-2 rounded-md flex items-center gap-x-2">
                   <FiCopy className="" />
                   <span className="text-neutral-700 dark:text-white font-semibold">
                       <a href="mailto:elrizah@gmail.com">Copy Email</a>
@@ -192,10 +191,10 @@ function page() {
             </div>
           </div>
 
-          <div className="bg-[#F6F8FA] p-6 dark:bg-[#2C2C2C]  mt-14 rounded-md">
+          <div className="bg-[#F6F8FA] p-6 dark:bg-[#2C2C2C] mt-14 rounded-md">
             <div className="flex items-center justify-between">
               <div className="flex items-center gap-x-2 font-InterRe">
-                <div className="w-[6px] h-[6px] rounded-full  bg-[#FB6D48]" />
+                <div className="w-[6px] h-[6px] rounded-full bg-[#FB6D48]" />
                 <h1 className="text-neutral-700 dark:text-[#C0C0C0] text-lg font-InterMe">
                   Follow Me
                 </h1>
@@ -214,20 +213,20 @@ function page() {
                 <a href="https://www.linkedin.com/in/elsarita-njoki-oulie-b22b3a291/">
                   <div className="border-neutral-300 bg-white shadow-md drop-shadow-md border dark:bg-[#373737] dark:border-none w-9 h-9 px-2 rounded-full flex items-center justify-center gap-x-1">
                     <FaLinkedinIn className="text-neutral-500 dark:text-white" />
-              </div>
-              </a>
+                  </div>
+                </a>
               </div>
             </div>
           </div>
 
-          <div className="bg-[#F6F8FA] dark:bg-[#2C2C2C] p-6  mt-4 rounded-md">
+          <div className="bg-[#F6F8FA] dark:bg-[#2C2C2C] p-6 mt-4 rounded-md">
             <div className="text-center">
               <span className="text-neutral-700 dark:text-neutral-400">
                 © 2024 Elsarita – elrizah@gmail.com
               </span>
               <p className="text-neutral-500">
                 By{""}
-                <span className="hover:underline  hover:text-neutral-400 text-neutral-700 dark:text-neutral-300">
+                <span className="hover:underline hover:text-neutral-400 text-neutral-700 dark:text-neutral-300">
                   kooya3
                 </span>
               </p>
@@ -239,4 +238,4 @@ function page() {
   );
 }
 
-export default page;
+export default Page;
